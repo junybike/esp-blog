80a2813452a8a250013e105406aeda49ea149436
jsn <leejunwoo99990@gmail.com>
2025-12-22 14:17:20 -0800

    p1 new example: control ledc with adc

--- esp32_E1/Control_LEDC_with_ADC/main/main.c
+++ esp32_E1/Control_LEDC_with_ADC/main/main.c
@@ -22,13 +22,37 @@ static const char *TAG = "APP";
     
 void app_main(void)
 {
+    ledc_timer_config_t timer = {
+        .speed_mode = LEDC_MODE,
+        .timer_num = LEDC_TIMER,
+        .duty_resolution = LEDC_DUTY_RES,
+        .freq_hz = LEDC_FREQUENCY,
+        .clk_cfg = LEDC_AUTO_CLK,
+    };
+    ledc_timer_config(&timer);
+
+    ledc_channel_config_t channel = {
+        .speed_mode = LEDC_MODE,
+        .channel = LEDC_CHANNEL,
+        .timer_sel = LEDC_TIMER,
+        .intr_type = LEDC_INTR_DISABLE,
+        .gpio_num = LEDC_OUTPUT_IO,
+        .duty = 0,
+    };
+    ledc_channel_config(&channel);
+
 	adc1_config_width(ADC_WIDTH_BIT_12);
     adc1_config_channel_atten(ADC_PIN, ADC_ATTEN_DB_11);
 
     while (1)
     {
         int val = adc1_get_raw(ADC_PIN);
-        ESP_LOGI(TAG, "ADC: %d", val);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        // ESP_LOGI(TAG, "ADC: %d", val);
+        val = val / 16;
+        
+        ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, val);
+        ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
+
+        vTaskDelay(100 / portTICK_PERIOD_MS);
     }
 }
